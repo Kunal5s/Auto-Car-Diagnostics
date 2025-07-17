f945242f21545a11d2a428affd4dec401b23f2a6
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-07-17 05:20:18 +0000

    Galat kiya tumhe white background our text colour black Hona chahiye the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚗</text></svg>" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
