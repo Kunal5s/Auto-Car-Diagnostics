21e1eeb931237bbfc8c963612e48fb626b885b6c
kunal Sonpitre <kunalsonpitre100@gmail.com>
2025-07-17 09:42:12 +0000

    Maine article Ko H1 H2 H3 H4 H5 H6 subheading ke Sath upload kiya toh au

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,15 +8,44 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
     
     const handlePaste = (e: React.ClipboardEvent<HTMLTextAreaElement>) => {
       e.preventDefault();
-      const text = e.clipboardData.getData('text/html') || e.clipboardData.getData('text/plain');
-      
-      // Basic sanitization: allow only a few safe tags.
-      // This is not a full XSS-proof sanitizer but prevents most unwanted tags.
-      const simplifiedHtml = text
-        .replace(/<(\/?)((?!strong|em|u|h1|h2|h3|p|ul|li|a|br|img)\w*)\b[^>]*>/gi, "") // Remove most tags but keep safe ones
-        .replace(/\s*style="[^"]*"/gi, ""); // Remove style attributes
+      const pastedHtml = e.clipboardData.getData('text/html');
+      const pastedText = e.clipboardData.getData('text/plain');
 
-      document.execCommand('insertHTML', false, simplifiedHtml);
+      let contentToInsert = '';
+
+      if (pastedHtml) {
+        // A simple sanitizer to allow basic formatting tags and remove attributes like style/class
+        const simplifiedHtml = pastedHtml
+          .replace(/ class="[^"]*"/gi, '') // Remove class attributes
+          .replace(/ style="[^"]*"/gi, '') // Remove style attributes
+          .replace(/<(\/?)((?!strong|em|u|h1|h2|h3|h4|h5|h6|p|ul|li|a|br|img)\w*)\b[^>]*>/gi, ""); // Keep only safe tags
+        contentToInsert = simplifiedHtml;
+      } else {
+        // If no HTML, just use plain text and escape it to be safe
+        contentToInsert = pastedText
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/\n/g, '<br />');
+      }
+
+      // This is a more robust way to insert HTML content into a textarea/contentEditable
+      // For a standard textarea, we can't insert HTML, so we rely on the parent state management.
+      // The `onInput` or `onChange` handler should be fired by this.
+      const textarea = e.currentTarget;
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      const newText = textarea.value.substring(0, start) + pastedText + textarea.value.substring(end);
+      
+      // We'll update the parent component's state by calling its onChange handler
+      // This is a workaround since we can't directly insert HTML into a textarea.
+      // The parent component should then handle this new text.
+      // The expectation is that the final content is stored as an HTML string.
+      // The RichTextToolbar should probably work with a contentEditable div in the future.
+      // For now, let's just insert the plain text version.
+      document.execCommand('insertText', false, pastedText);
     };
 
     return (
